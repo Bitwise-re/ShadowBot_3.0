a2b3adf408d3e6445d97571d0bf73b092892e773
C0dex73 <c0d3x73@gmail.com>
2025-02-08 17:06:45 +0100

    hello world and template clean

--- src/utils/event.ts
+++
@@ -1,9 +0,0 @@
-import ClientWithCommands from './clientWithCommands';
-import { Awaitable } from 'discord.js';
-
-/**
- * The architecture of an event object (modules of the event/ directory).
- */
-export default interface Ievent{
-    listener : (bot:ClientWithCommands, ...args : any[]) => Awaitable<void>;
-}
